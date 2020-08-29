ee5564eda34ac5c63d28c2eb79d7cb992b67a6da
Ashish Gupta <gupta.ashish@lattukids.com>
2020-08-29 11:54:50 +0530

    renamed view model

--- app/src/main/java/com/example/greedygameimagecachinglibrary/bll/MainActivityViewModel.kt
+++ app/src/main/java/com/example/greedygameimagecachinglibrary/bll/MainActivityViewModel.kt
@@ -2,10 +2,11 @@ package com.example.greedygameimagecachinglibrary.bll
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.greedygameimagecachinglibrary.dal.MainActivityRepository
 import com.example.greedygameimagecachinglibrary.network.Resource
 import com.example.greedygameimagecachinglibrary.pojo.CompleteImageData
 
-class MainActivityViewModel(private val mainActivityRepository: MainActivityViewModel) :ViewModel() {
+class MainActivityViewModel(private val mainActivityRepository: MainActivityRepository) :ViewModel() {
 
     fun getAllImageResponse(): MutableLiveData<Resource<List<CompleteImageData>>> {
             return mainActivityRepository.getAllImageResponse()
