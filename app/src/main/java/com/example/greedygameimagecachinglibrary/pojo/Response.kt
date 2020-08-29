8ea3a0e8b680b471e34a4386575195af63fb9c67
Ashish Gupta <gupta.ashish@lattukids.com>
2020-08-29 11:24:13 +0530

    created pojo for response

---
+++ app/src/main/java/com/example/greedygameimagecachinglibrary/pojo/Response.kt
@@ -0,0 +1,9 @@
+package com.example.greedygameimagecachinglibrary.pojo
+
+data class Response(val kind: String, val data: ListImageData)
+data class ListImageData(val dist:Int, val children:List<CompleteImageData>)
+data class CompleteImageData(val kids:String, val data:SmallImageData)
+data class SmallImageData(val thumbnail:String, val preview:ImagePreview)
+data class ImagePreview(val images:List<Images>)
+data class Images(val source:SingleImage, val resolutions:List<SingleImage>)
+data class SingleImage(val url:String, val width:Int, val height:String)
